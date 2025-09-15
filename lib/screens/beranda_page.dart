21b51ce238e1bbe119b85dc3944f7d698e5f1630
Akbar Setiyawan <offizier.rezy@gmail.com>
2025-09-15 12:59:11 +0700

    update

--- lib/screens/beranda_page.dart
+++ lib/screens/beranda_page.dart
@@ -7,6 +7,16 @@ class BerandaPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Beranda',
+            style: TextStyle(fontWeight: FontWeight.bold)),
+        backgroundColor: Colors.black,
+        iconTheme: const IconThemeData(color: Colors.white),
+        titleTextStyle: const TextStyle(
+            color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20),
+        centerTitle: true,
+        elevation: 2,
+      ),
       body: InAppWebView(
         initialUrlRequest:
             URLRequest(url: WebUri('https://jdih.madiunkota.go.id/')),
